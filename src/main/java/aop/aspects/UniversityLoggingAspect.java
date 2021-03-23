36416dd1ba021c0e9ed2e69940be66579c7bab38
Alexey Alekhin <nihkela@gmail.com>
2021-03-23 22:51:12 +0300

    Add @AfterThrowing advice

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -2,6 +2,7 @@ package aop.aspects;
 
 import aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -17,19 +18,25 @@ public class UniversityLoggingAspect {
                 "перед методом getStudents");
     }
 
-    @AfterReturning(value = "execution(* getStudents())", returning = "students")
-    public void afterReturningGetStudentLoggingAdvice(List<Student> students) {
-        Student firstStudent = students.get(0);
-        String nameSurname = firstStudent.getNameSurname();
-        nameSurname = "Mr." + nameSurname;
-        firstStudent.setNameSurname(nameSurname);
+//    @AfterReturning(value = "execution(* getStudents())", returning = "students")
+//    public void afterReturningGetStudentLoggingAdvice(List<Student> students) {
+//        Student firstStudent = students.get(0);
+//        String nameSurname = firstStudent.getNameSurname();
+//        nameSurname = "Mr." + nameSurname;
+//        firstStudent.setNameSurname(nameSurname);
+//
+//        double avgGrade = firstStudent.getAvgGrade();
+//        avgGrade = avgGrade + 1;
+//        firstStudent.setAvgGrade(avgGrade);
+//
+//
+//        System.out.println("beforeGetStudentLoggingAdvice: логируем получение списка студентов " +
+//                "после работы метода getStudents");
+//    }
 
-        double avgGrade = firstStudent.getAvgGrade();
-        avgGrade = avgGrade + 1;
-        firstStudent.setAvgGrade(avgGrade);
+    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
 
-
-        System.out.println("beforeGetStudentLoggingAdvice: логируем получение списка студентов " +
-                "после работы метода getStudents");
+        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
     }
 }
