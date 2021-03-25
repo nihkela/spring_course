50aeaeab108619e93a67319b279eeece4d03293d
Alexey Alekhin <nihkela@gmail.com>
2021-03-25 20:22:05 +0300

    add @GeneratedValue

--- src/main/java/hibernate_test/Test.java
+++ src/main/java/hibernate_test/Test.java
@@ -14,11 +14,13 @@ public class Test {
 
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Zaur", "Tregulov", "IT", 500);
+            Employee emp = new Employee("Aleksandr", "Smirnov", "Sales", 700);
 
             session.beginTransaction();
             session.save(emp);
             session.getTransaction().commit();
+
+            System.out.println("Done!");
         } finally {
             factory.close();
         }
