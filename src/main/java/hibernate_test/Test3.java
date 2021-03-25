0659453766694c90c97c4368a9a90040900d8510
Alexey Alekhin <nihkela@gmail.com>
2021-03-25 22:02:14 +0300

    Add update statement

---
+++ src/main/java/hibernate_test/Test3.java
@@ -0,0 +1,34 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+
+            session.beginTransaction();
+
+            Employee emp = session.get(Employee.class, 1);
+//            emp.setSalary(1500);
+            session.createQuery("UPDATE Employee set salary = 1000" +
+                    "WHERE name = 'Aleksandr'").executeUpdate();
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
