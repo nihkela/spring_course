affcf4e12911d03467361865b04b8ba96c8bf766
Alexey Alekhin <nihkela@gmail.com>
2021-03-26 15:28:57 +0300

    Create Bi-Directional @OneToMany and @ManyToOne

---
+++ src/main/java/hibernate_one_to_many_bi/Test.java
@@ -0,0 +1,56 @@
+package hibernate_one_to_many_bi;
+
+import hibernate_one_to_many_bi.entity.Department;
+import hibernate_one_to_many_bi.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+
+
+        Session session = null;
+
+        try {
+//            session = factory.getCurrentSession();
+//
+//            Department department = new Department("IT", 300, 1200);
+//            Employee emp1 = new Employee("Zaur", "Tregulov", 500);
+//            Employee emp2 = new Employee("Elena", "Smirnova", 1000);
+//
+//            department.addEmployeeToDepartment(emp1);
+//            department.addEmployeeToDepartment(emp2);
+//
+//            session.beginTransaction();
+//
+//            session.save(department);
+
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//
+//            Employee employee = session.get(Employee.class, 1);
+//            System.out.println(employee);
+//            System.out.println(employee.getDepartment());
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, 1);
+            session.delete(employee);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
